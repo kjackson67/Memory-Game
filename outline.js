54496e3b55d509b728bb30a0e23d368135e3bb33
“Korby <jacksonkorbyr@johndeere.com>
2020-07-10 13:13:47 -0500

    first commit

---
+++ outline.js
@@ -0,0 +1,27 @@
+//      USER STORY OUTLINE
+// PLayers can play game 'flash cards'
+// As a player- I want to click on choice of 2 random cards
+// As a player - I want to match the 2 cards
+// I want to hear a click on my card choices
+// I want to keep track of how many I get correct
+// I want to keep track of how many turns I take
+// I want it to let me know if I don't match 2 cards
+// I want to see a celebration if I get all matched in one turn
+// I want to be able to reset or restart game at anytime
+// I want to increase levels (add more cards) when a level is completed
+//  
+//      PSEUDO CODE OUTLINE    
+//  Create HTML
+// create layout for 10 rectangular spaces
+// create header box for game name
+// create a start game button/box
+// create box for number of turns 
+// create box for how many correct
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
